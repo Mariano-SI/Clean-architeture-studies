f928f82d52940413af0e1f22baab1fc41d97abf5
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-11 15:00:47 -0300

    feat: swagger setup

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -2,11 +2,27 @@ import express from 'express'
 import cors from 'cors'
 import { routes } from './routes'
 import { errorHandler } from './middlewares/errorHandler'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express'
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Clean Arch Documentation',
+      version: '1.0.0',
+    },
+  },
+  apis: [],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 app.use(routes)
 app.use(errorHandler)
 
