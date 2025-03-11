3077bf527e627ff413758edcd1ab87798411ab65
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-11 14:33:10 -0300

    feat: configurando validacao sintatica e tipagem as envs do projeto

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,7 +1,6 @@
+import { env } from '../env'
 import app from './app'
 
-const port = process.env.PORT || 3333
-
-app.listen(port, () => {
-  console.log('Server is running on port ', port)
+app.listen(env.PORT, () => {
+  console.log('Server is running on port ', env.PORT)
 })
