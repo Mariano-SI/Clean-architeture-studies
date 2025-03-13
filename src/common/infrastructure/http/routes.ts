7e519ed377823816fcb190ad944347153331dced
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-12 21:58:41 -0300

    feat: rotas de produtos definidas

--- src/common/infrastructure/http/routes.ts
+++ src/common/infrastructure/http/routes.ts
@@ -1,3 +1,4 @@
+import { productsRouter } from '@/products/infrastructure/http/routes/products.routes'
 import { Router } from 'express'
 
 const routes = Router()
@@ -5,5 +6,7 @@ const routes = Router()
 routes.get('/', (req, res) => {
   return res.json({ message: 'Olá, Dev!' })
 })
+
+routes.use('/products', productsRouter)
 
 export { routes }
