47edc013bff635296f4d26dc33e4c9d68f6b0dd4
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-13 16:26:58 -0300

    feat: controller para listagem de produto

---
+++ src/products/infrastructure/http/controllers/list-product.controller.ts
@@ -0,0 +1,33 @@
+import { BadRequestError } from '@/common/domain/errors/bad-reques-error'
+import { Request, Response } from 'express'
+import { z } from 'zod'
+import { ProductRepositoryPG } from '../../database/product.repository.pg'
+import { ListProductUsecase } from '@/products/application/usecases/list-product.usecase'
+
+export async function listProductController(
+  req: Request,
+  res: Response,
+): Promise<Response> {
+  const listProductBodySchema = z.object({
+    id: z.string().uuid('O ID deve ser um UUID válido'),
+  })
+
+  const validatedData = listProductBodySchema.safeParse(req.params)
+
+  if (!validatedData.success) {
+    throw new BadRequestError(
+      `${validatedData.error.errors.map(error => {
+        return `${error.path} -> ${error.message}`
+      })}`,
+    )
+  }
+
+  const { id } = validatedData.data
+
+  const productRepository = ProductRepositoryPG.getInstance()
+  const listProductUsecase = new ListProductUsecase(productRepository)
+
+  const product = await listProductUsecase.execute({ id })
+
+  return res.status(200).json(product)
+}
