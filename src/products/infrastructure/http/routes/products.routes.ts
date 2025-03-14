367f05d6fe09dbc263bb11cd5e7146afa0946a96
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-14 10:26:08 -0300

    feat: documetação do endpoint de delete product

--- src/products/infrastructure/http/routes/products.routes.ts
+++ src/products/infrastructure/http/routes/products.routes.ts
@@ -153,6 +153,26 @@ productsRouter.get('/:id', listProductController)
  */
 
 productsRouter.patch('/:id', updateProductController)
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Delete a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product ID
+ *     responses:
+ *       204:
+ *         description: The product was successfully deleted
+ *       404:
+ *         description: The product was not found
+ */
 productsRouter.delete('/:id', deleteProductController)
 
 export { productsRouter }
