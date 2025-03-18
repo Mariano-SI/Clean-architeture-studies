2107dcfca4088f7733a40145a5c65571a4295dd3
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-17 22:05:19 -0300

    criação de token no controller

--- src/users/infrastructure/http/controllers/authenticate-user.controller.ts
+++ src/users/infrastructure/http/controllers/authenticate-user.controller.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { UsersRepositoryPG } from '../../database/user.repository.pg'
 import { AuthenticateUserUsecase } from '@/users/application/usecases/authenticate-user.usecase'
 import { HashProviderBcryptjs } from '@/common/infrastructure/providers/hash-provider.bcryptjs'
+import { AuthTokenProviderJsonwebtoken } from '@/common/infrastructure/providers/auth-token-provider.jsonwebtoken'
+import { access } from 'fs'
 
 export async function authenticateUserController(
   req: Request,
@@ -33,7 +35,13 @@ export async function authenticateUserController(
     hashProvider,
   )
 
-  const user = await authenticateUserUsecase.execute({ email, password }) //todo: change this variable name after implements the jwt provider
+  const user = await authenticateUserUsecase.execute({ email, password })
 
-  return res.status(200).json(user)
+  const authTokenProvider = new AuthTokenProviderJsonwebtoken()
+
+  const { access_token } = authTokenProvider.generateAuthToken(user.id)
+
+  return res.status(200).json({
+    access_token,
+  })
 }
