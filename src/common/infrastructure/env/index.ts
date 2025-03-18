762d462b654a62044d5df6e336db53d447f10c0c
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-17 21:28:59 -0300

    feat: adicionando e validando novas variaveis

--- src/common/infrastructure/env/index.ts
+++ src/common/infrastructure/env/index.ts
@@ -15,6 +15,8 @@ const envSchema = z.object({
   DB_NAME: z.string().default('postgres'),
   DB_USER: z.string().default('postgres'),
   DB_PASS: z.string().default('postgres'),
+  JWT_TOKEN: z.string(),
+  JWT_EXPIRES_IN: z.string().default('86400'),
 })
 
 const _env = envSchema.safeParse(process.env)
