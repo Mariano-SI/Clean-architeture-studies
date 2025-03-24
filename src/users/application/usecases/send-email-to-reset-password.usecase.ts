3fa13dd0077c3b628708c9aa61a649b49e326e1e
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-24 20:38:14 -0300

    feat: caso de uso para envio de email para reset de senha

---
+++ src/users/application/usecases/send-email-to-reset-password.usecase.ts
@@ -0,0 +1,34 @@
+import { NotFoundError } from '@/common/domain/errors/not-found-error'
+import { UserModel } from '@/users/domain/models/users.model'
+import { UserTokensRepository } from '@/users/domain/repositories/user-tokens.repository'
+import { UsersRepository } from '@/users/domain/repositories/users.repository'
+
+interface Input {
+  email: string
+}
+
+interface Output {
+  user: UserModel
+  token: string
+}
+
+export class SendEmailToResetPasswordUsecase {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly userTokensRepository: UserTokensRepository,
+  ) {}
+
+  async execute(input: Input): Promise<Output> {
+    const user = await this.usersRepository.findByEmail(input.email)
+
+    if (!user) {
+      throw new NotFoundError('User not found')
+    }
+
+    const { token } = await this.userTokensRepository.create({
+      user_id: user.id,
+    })
+
+    return { user, token }
+  }
+}
