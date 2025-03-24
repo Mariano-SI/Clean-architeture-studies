849b6f55963424f0b96ad77cc407f831b86be9e4
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-24 20:58:48 -0300

    feat: reset password usecase

---
+++ src/users/application/usecases/reset.password.usecase.ts
@@ -0,0 +1,43 @@
+import { BadRequestError } from '@/common/domain/errors/bad-request-error'
+import { NotFoundError } from '@/common/domain/errors/not-found-error'
+import { HashProvider } from '@/common/domain/providers/hash-provider'
+import { UserTokensRepository } from '@/users/domain/repositories/user-tokens.repository'
+import { UsersRepository } from '@/users/domain/repositories/users.repository'
+import { addHours, isAfter } from 'date-fns'
+
+interface Input {
+  token: string
+  password: string
+}
+
+export class ResetPasswprdUsecase {
+  constructor(
+    private readonly userRepository: UsersRepository,
+    private readonly userTokensRepository: UserTokensRepository,
+    private readonly hashProvider: HashProvider,
+  ) {}
+
+  async execute(input: Input): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(input.token)
+
+    if (!userToken) {
+      throw new NotFoundError('User token not found')
+    }
+
+    const user = await this.userRepository.findById(userToken.user_id)
+
+    if (!user) {
+      throw new NotFoundError('User not found')
+    }
+
+    const compareDate = addHours(userToken.created_at, 2)
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new BadRequestError('Token expired')
+    }
+
+    user.password = await this.hashProvider.generateHash(input.password)
+
+    await this.userRepository.update(user)
+  }
+}
