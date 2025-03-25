20a7822092c906acdfc6c9028e8d9d2f9812c0e3
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-03-24 21:04:20 -0300

    feat: rota para reset de senha

--- src/users/infrastructure/http/routes/password.routes.ts
+++ src/users/infrastructure/http/routes/password.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { sendEmailToResetPasswordController } from '../controllers/send-email-to-reset-password.controller'
+import { resetPasswordController } from '../controllers/reset-password.controller'
 
 const passwordRoutes = Router()
 
 passwordRoutes.post('/forgot', sendEmailToResetPasswordController)
+passwordRoutes.post('/reset', resetPasswordController)
 
 export { passwordRoutes }
